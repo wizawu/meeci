a24c3b3d31648313290c58815714881387f25f08
wizawu <wizawu@gmail.com>
2014-03-14 16:44:26 +0800

    change to sqlite

--- initdb.sql
+++ initdb.sql
@@ -1,47 +1,67 @@
--- PostgreSQL
+-- SQLite version 3 --
 
-CREATE TABLE IF NOT EXISTS user (
-    user      VARCHAR(32), 
+-- user --
+CREATE TABLE user (
+    id        INTEGER PRIMARY KEY,
+    user      VARCHAR(32),
     name      VARCHAR(64),
-    email     VARCHAR(64),
-    passwd    CHAR(32),
-    token     CHAR(16),
-
-    PRIMARY KEY (user)
+    email     VARCHAR(128),
+    passwd    CHAR(32)
 );
 
-CREATE UNIQUE INDEX user_email_index ON user (email);
+CREATE UNIQUE INDEX user_user_idx ON user(user);
+CREATE UNIQUE INDEX user_email_idx ON user(email);
 
-CREATE TABLE IF NOT EXISTS build (
-    id            SERIAL,
-
-    service       SMALLINT,
-    owner         TEXT,
-    repository    TEXT,
-    build         INTEGER,
-
-    branch        TEXT,
-    commit        TEXT,
-    committer     TEXT,
-    message       TEXT,
-
-    container     TEXT,
-    worker        TEXT,
-
-    start         TIMESTAMP,
-    stop          TIMESTAMP,
-    return        SMALLINT,
-
-    PRIMARY KEY (id),
-    UNIQUE (service, owner, repository, build),
+INSERT INTO user(id, user, name, email, passwd) VALUES(
+    1000000000, 'root', 'root', 'wizawu@gmail.com', '63a9f0ea7bb98050796b649e85481845'
 );
 
-CREATE INDEX build_repo_index ON build (service, owner, repository);
+-- build --
+CREATE TABLE build (
+    id           INTEGER PRIMARY KEY,
+    
+    host         SMALLINT,
+    owner        TEXT,
+    repos        TEXT,
+    build        INTEGER,
 
-CREATE TABLE IF NOT EXISTS repository (
-    service       SMALLINT,
-    owner         TEXT,
-    repository    TEXT,
+    branch       TEXT,
+    commit       TEXT,
+    committer    TEXT,
+    message      TEXT,
 
-    PRIMARY KEY (service, owner, repository)
+    container    TEXT,
+    worker       TEXT,
+
+    start        TEXT,
+    duration     INTEGER,
+    return       SMALLINT 
 );
+
+CREATE INDEX build_repos_idx ON build(repos, owner, host);
+
+-- repos --
+CREATE TABLE repos (
+    id           INTEGER PRIMARY KEY,
+    user         TEXT,
+    host         SMALLINT,
+    owner        TEXT,
+    repos        TEXT,
+    desc         TEXT,
+    container    TEXT,
+    script       TEXT
+);
+
+CREATE UNIQUE INDEX repos_user_idx ON repos(user, repos, owner, host);
+
+-- container --
+CREATE TABLE container (
+    id      INTEGER PRIMARY KEY,
+    user    TEXT,
+    name    VARCHAR(64),
+    desc    TEXT,
+    size    INTEGER,
+    time    TEXT
+);
+
+CREATE UNIQUE INDEX container_name_idx ON container(user, name);
