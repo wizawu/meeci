8a32aff74ed44a63b829ba37def3d2914b3e66f8
wizawu <wizawu@gmail.com>
2014-03-17 01:46:10 +0800

    add home.html

--- web/app.js
+++ web/app.js
@@ -1,7 +1,20 @@
 var express = require('express');
 var app = express();
 
-var http = require('http');
-var port = process.env.MEECI_PORT || 8037;
+app.use(express.static('./public'));
+app.use(express.logger({
+    format: ':remote-addr - - [:date] ":method :url HTTP/:http-version" '+
+            ':status :res[content-length] - :response-time ms'
+}));
+app.use(express.errorHandler());
 
-http.createServer(app).listen(port);
+app.get('/', function(req, res) {
+    res.sendfile('test.html');
+});
+
+var http = require('http');
+var port = process.env.MEECI_PORT || 3780;
+
+http.createServer(app).listen(port, function() {
+    console.log('Meeci Web is listening on port ' + port);
+});
