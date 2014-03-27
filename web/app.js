a4e4115364784c22c2ec4e26ebcdc089a69684d4
wizawu <wizawu@gmail.com>
2014-03-28 02:02:05 +0800

    changes several days ago

--- web/app.js
+++ web/app.js
@@ -1,20 +1,34 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
-app.use(express.static('./public'));
+app.use(express.static("./public"));
 app.use(express.logger({
     format: ':remote-addr - - [:date] ":method :url HTTP/:http-version" '+
             ':status :res[content-length] - :response-time ms'
 }));
 app.use(express.errorHandler());
+app.use(express.bodyParser());
 
-app.get('/', function(req, res) {
-    res.sendfile('home.html');
+app.get("/", function(req, res) {
+    res.sendfile("home.html");
 });
 
-var http = require('http');
+app.get("/user", function(req, res) {
+
+});
+
+var http = require("http");
 var port = process.env.MEECI_PORT || 3780;
 
 http.createServer(app).listen(port, function() {
-    console.log('Meeci Web is listening on port ' + port);
+    console.log("Meeci Web is listening on port " + port);
 });
+
+/**
+ * Internal Functions
+ */
+function encrypt(passwd) {
+    var salt = crypto.randomBytes(32).toString("base64");
+    var hash = crypto.createHash("sha256").update(salt+passwd).digest("base64");
+    return { hash: hash, salt: salt };
+}
--- web/public/scripts/default.js
+++ web/public/scripts/default.js
@@ -1,44 +1,57 @@
+/**
+ * Switch Tabs
+ */
 function activeTab(id) {
     var tabs = document.getElementById("tab-content").childNodes;
     var n = tabs.length;
     for (var i = 0; i < n; i++) {
-        if (tabs[i].nodeName.toLowerCase() != "div") {
-            continue;
-        }
-        if (tabs[i].id == id) {
-            tabs[i].style.display = "block";
-        } else {
-            tabs[i].style.display = "none";
+        var t = tabs[i];
+        if (t.nodeName.toLowerCase() == "div") {
+            t.style.display = (t.id == id)? "block":"none";
         }
     }
 }
 
-function addTabClickEvent() {
-    var list = document.getElementsByClassName("nav nav-tabs")[0].childNodes;
+function addClickTabEvent() {
+    var list = document.getElementById("tab-titles").childNodes;
     var n = list.length;
     for (var i = 0; i < n; i++) {
-        if (list[i].nodeName.toLowerCase() != "li") {
-            continue;
-        }
-        list[i].index = i;
-        list[i].childNodes[1].onclick = function() {
-            for (var j = 0; j < n; j++) {
-                if (list[j].nodeName.toLowerCase() == "li") {
-                    list[j].className = "";
+        var li = list[i];
+        if (li.nodeName.toLowerCase() == "li") {
+            li.index = i;
+            li.childNodes[1].onclick = function() {
+                for (var j = 0; j < n; j++) {
+                    if (list[j].nodeName.toLowerCase() == "li") {
+                        list[j].className = "";
+                    }
                 }
-            }
-            var k = this.parentNode.index;
-            list[k].className = "active";
-            activeTab(list[k].getAttribute("tabid"));
-            return false;
-        };
+                j = this.parentNode.index;
+                list[j].className = "active";
+                activeTab(list[j].getAttribute("tabid"));
+                return false;
+            };
+        }
+    }
+}
+
+/**
+ * Hide all elements not in common
+ */
+function hideAll() {
+    var ids = [
+        "form-login", "profile",
+        "li-repos", "li-container", "li-account", "li-signup"
+    ];
+    for (var i = 0; i < ids.length; i++) {
+        document.getElementById(ids[i]).style.display = "none";
     }
 }
 
 function addEvents() {
-    addTabClickEvent();
+    addClickTabEvent();
 }
 
 window.onload = function() {
+    //hideAll(); 
     addEvents();
 }
