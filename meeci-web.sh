7fed7cb7eafa4d091c5351c90d78feaf61dfdb2d
wizawu <wizawu@gmail.com>
2014-03-28 02:06:08 +0800

    start services

--- meeci-web.sh
+++ meeci-web.sh
@@ -6,4 +6,10 @@ if [[ $1 == "-h" || $# -ne 1 ]]; then
 fi 
 
 sudo mount --bind /var/lib/meeci/containers/ /srv/ftp/meeci/containers/
+sudo /etc/init.d/vsftpd restart
+
+if [[ `pgrep memcached` == '' ]]; then
+    sudo /etc/init.d/memcached restart
+fi
+
 sudo MEECI_PORT=$1 node web/app
