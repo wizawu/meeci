ff6d5df134aaab160438414c2880d51f8790acdd
wizawu <wizawu@gmail.com>
2014-03-31 15:18:47 +0800

    sign-up ok

--- web/app.js
+++ web/app.js
@@ -1,5 +1,12 @@
+var crypto = require("crypto");
+var util = require("util");
 var express = require("express");
+var pg = require("pg.js");
+
+var strformat = util.format;
+
 var app = express();
+var pgdb = "postgres://meeci:IoMQwf5E7u8@0.0.0.0/meeci";
 
 app.use(express.static("./public"));
 app.use(express.logger({
@@ -8,13 +15,55 @@ app.use(express.logger({
 }));
 app.use(express.errorHandler());
 app.use(express.bodyParser());
+app.use(express.cookieParser(crypto.randomBytes(64).toString('base64')));
+app.use(express.session());
 
 app.get("/", function(req, res) {
     res.sendfile("home.html");
 });
 
-app.get("/user", function(req, res) {
+app.post("/signup", function(req, res) {
+    var body = req.body;
+    var user = body.user, email = body.email, passwd = body.passwd;
+    if (user && email && passwd) {
+        if (user[0] >= '0' && user[0] <= '9') {
+            res.send(401);
+        } else if (! checkLetter(user)) {
+            res.send(401);
+        } else if (! checkLetter(email, "@-.")) {
+            res.send(402);
+        } else if (! checkLetter(passwd)) {
+            res.send(403);
+        } else {
+            var cond = strformat("user_ = '%s'", user);
+            sql_exist("users", cond, function() {
+                res.send(401);
+            });
+            var cond = strformat("email = '%s'", email);
+            sql_exist("users", cond, function() {
+                res.send(402);
+            });
+            passwd = encrypt(passwd);
+            var values = strformat(
+                "'%s', '%s', '%s', '%s', 1",
+                user, email, passwd.hash, passwd.salt
+            );
+            sql_insert(
+                "users", "user_, email, passwd, salt, status",
+                values, function(code) { res.send(code); }
+            );
+        }
+    } else {
+        res.send(400);
+    }
+});
 
+app.get("/user", function(req, res) {
+    if (req.session.user == null) {
+        res.json({"user": null});
+    } else {
+        res.json({"user": "wiza"})
+    }
 });
 
 var http = require("http");
@@ -24,11 +73,57 @@ http.createServer(app).listen(port, function() {
     console.log("Meeci Web is listening on port " + port);
 });
 
-/**
- * Internal Functions
- */
+/* Internal Functions */
+function errlog(err) {
+    return console.error(err);
+}
+
+function checkLetter(str, extra) {
+    for (var i in str) {
+        var c = str[i];
+        if (c >= 'a' && c <= 'z') continue;
+        else if (c >= 'A' && c <= 'Z') continue;
+        else if (c >= '0' && c <= '9') continue;
+        else if (c == '_') continue;
+        else if (extra && extra.search(c) >= 0) continue;
+        else return false;
+    }
+    return true;
+}
+
 function encrypt(passwd) {
     var salt = crypto.randomBytes(32).toString("base64");
     var hash = crypto.createHash("sha256").update(salt+passwd).digest("base64");
     return { hash: hash, salt: salt };
+}
+
+function sql_exist(table, cond, callback) {
+    var q = strformat("SELECT 1 FROM %s WHERE %s;", table, cond);
+    pg.connect(pgdb, function(err, client, release) {
+        if (err) return errlog(err);
+        client.query(q, function(err, res) {
+            release();
+            if (err) {
+                return errlog(err);
+            } else if (res.rows.length > 0) {
+                callback();
+            }
+        });
+    });
+}
+
+function sql_insert(table, fields, values, callback) {
+    var q = strformat("INSERT INTO %s(%s) VALUES(%s)", table, fields, values);
+    pg.connect(pgdb, function(err, client, release) {
+        if (err) return errlog(err);
+        client.query(q, function(err, res) {
+            release();
+            if (err) {
+                callback(500);
+                return errlog(err);
+            } else {
+                callback(200);
+            }
+        });
+    });
 }
--- web/public/scripts/default.js
+++ web/public/scripts/default.js
@@ -1,7 +1,49 @@
-/**
- * Switch Tabs
- */
+/* Backbone models */
+SignUp = Backbone.Model.extend({
+    url: "/signup"
+});
+
+User = Backbone.Model.extend({
+    defaults: {
+        "user" : null,
+        "name" : null,
+        "email": null
+    },
+    url: "/user"
+});
+
+var user = new User;
+
+/* Return the first child with the specified tag name */
+function getChildByTagName(node, tag) {
+    var children = node.childNodes;
+    var n = children.length;
+    for (var i = 0; i < n; i++) {
+        if (children[i].nodeName.toLowerCase() == tag) {
+            return children[i];
+        }
+    }
+}
+
+function displayElementById(id, attr) {
+    if (!attr) attr = "block";
+    document.getElementById(id).style.display = attr;
+}
+
+/* Change the tab content */
 function activeTab(id) {
+    var list = document.getElementById("tab-titles").childNodes;
+    var n = list.length;
+    for (var i = 0; i < n; i++) {
+        if (list[i].nodeName.toLowerCase() == "li") {
+            if (list[i].getAttribute("tabid") == id) {
+                list[i].className = "active";
+            } else {
+                list[i].className = "";
+            }
+        }
+    }
+
     var tabs = document.getElementById("tab-content").childNodes;
     var n = tabs.length;
     for (var i = 0; i < n; i++) {
@@ -18,40 +60,106 @@ function addClickTabEvent() {
     for (var i = 0; i < n; i++) {
         var li = list[i];
         if (li.nodeName.toLowerCase() == "li") {
-            li.index = i;
-            li.childNodes[1].onclick = function() {
-                for (var j = 0; j < n; j++) {
-                    if (list[j].nodeName.toLowerCase() == "li") {
-                        list[j].className = "";
-                    }
-                }
-                j = this.parentNode.index;
-                list[j].className = "active";
-                activeTab(list[j].getAttribute("tabid"));
+            getChildByTagName(li, "a").onclick = function() {
+                activeTab(this.parentNode.getAttribute("tabid"));
                 return false;
             };
         }
     }
 }
 
-/**
- * Hide all elements not in common
- */
+/* Hide all elements not in common */
 function hideAll() {
     var ids = [
         "form-login", "profile",
         "li-repos", "li-container", "li-account", "li-signup"
     ];
     for (var i = 0; i < ids.length; i++) {
-        document.getElementById(ids[i]).style.display = "none";
+        displayElementById(ids[i], "none");
     }
 }
 
+/* Decide which to display on top-right corner */
+function setDivAccount() {
+    user.fetch({
+        success: function(model, response, options) {
+            if (model.get("user")) {
+                displayElementById("form-login", "none");
+                displayElementById("profile", "block");
+            } else {
+                displayElementById("form-login", "block");
+                displayElementById("profile", "none");
+            }
+        }
+    });
+}
+
+/* Switch to Join-Now tab */
+function addJoinEvent() {
+    var b = document.getElementById("join-now");
+    b.onclick = function() {
+        displayElementById("li-signup");
+        activeTab("tab-signup");
+    };
+}
+
+function addSignUpEvent() {
+    var f = getChildByTagName(document.getElementById("tab-signup"), "form");
+    var b = getChildByTagName(f, "button");
+    var inputs = ["sign-user", "sign-email", "sign-pswd1", "sign-pswd2"];
+    for (var i in inputs) {
+        inputs[i] = document.getElementById(inputs[i]);
+    }
+    b.onclick = function() {
+        if (inputs[2].value != inputs[3].value) {
+            alert("The two passwords are not consistent.");
+            inputs[2].value = inputs[3].value = "";
+            return false;
+        }
+        var signup = new SignUp({
+            user   : inputs[0].value.trim(),
+            email  : inputs[1].value.trim(),
+            passwd : inputs[2].value.trim()
+        });
+        signup.save(signup.attributes, {
+            error: function(model, response, options) {
+                switch (response.status) {
+                    case 200:
+                        alert("You have signed up successfully!");
+                        for (var i in inputs) inputs[i].value = "";
+                        break;
+                    case 400:
+                        alert("Incomplete sign-up form.");
+                        break;
+                    case 401:
+                        alert("Please use another username.");
+                        inputs[0].value = "";
+                        break;
+                    case 402:
+                        alert("Please use another email.");
+                        inputs[1].value = "";
+                        break;
+                    case 403:
+                        alert("Invalid password.");
+                        inputs[2].value = inputs[3].value = "";
+                        break;
+                    default:
+                        alert(response.status + " " + response.statusText);
+                }
+            }
+        });
+    }
+}
+
+/* Gather all add-event functions */
 function addEvents() {
     addClickTabEvent();
+    addJoinEvent();
+    addSignUpEvent();
 }
 
 window.onload = function() {
-    //hideAll(); 
+    hideAll();
+    setDivAccount();
     addEvents();
 }
