9148cf7b581a9299f7b9721c75709dc9af9f04e5
wizawu <wizawu@gmail.com>
2014-05-03 12:07:21 +0800

    migrate minbase.sh to meeci-worker

--- setup.sh
+++ setup.sh
@@ -8,13 +8,12 @@ fi
 set -x -e
 
 apt-get install -y --no-install-recommends \
-                debootstrap memcached nodejs vsftpd
+                memcached nodejs vsftpd
 
+mkdir -p /srv/ftp/meeci
 mkdir -p /var/lib/meeci/logs/container
 mkdir -p /var/lib/meeci/logs/build
 mkdir -p /var/lib/meeci/containers
-mkdir -p /srv/ftp/meeci
-
 chmod -R 777 /var/lib/meeci
 
 set +x
@@ -22,6 +21,4 @@ echo "Perform the following steps manually:"
 echo "[1] install PostgreSQL"
 echo "[2] uncomment 'write_enable=YES' and 'anon_upload_enable=YES' in /etc/vsftpd.conf"
 echo "[3] sudo /etc/init.d/vsftpd restart"
-
-set +x
 echo "Then you can start meeci-web with: ./meeci-web.sh 80"
